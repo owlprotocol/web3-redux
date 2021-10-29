09472a0bb213ea4c6689990f08c068b543019ccf
Leo Vigna <leo.vigna@gmail.com>
2021-10-29 16:03:40 -0400

    fetchNonce unit test

--- src/account/sagas/fetchNonce.ts
+++ src/account/sagas/fetchNonce.ts
@@ -1,5 +1,4 @@
 import { put, call } from 'typed-redux-saga/macro';
-import { Network } from '../../network/model';
 import networkExists from '../../network/sagas/networkExists';
 import { getIdDeconstructed } from '../model/interface';
 import { set, FetchNonceAction } from '../actions';
@@ -7,12 +6,13 @@ import exists from './exists';
 
 export function* fetchNonce(action: FetchNonceAction) {
     const { payload } = action;
-    const { networkId, address } = getIdDeconstructed(payload);
-    const network: Network = yield* call(networkExists, networkId);
-    if (!network.web3) throw new Error(`Network ${networkId} missing web3`);
     yield* call(exists, payload);
 
+    const { networkId, address } = getIdDeconstructed(payload);
+    const network = yield* call(networkExists, networkId);
     const web3 = network.web3;
+    if (!web3) throw new Error(`Network ${networkId} missing web3`);
+
     //@ts-expect-error
     const nonce: string = yield* call(web3.eth.getTransactionCount, address);
     yield* put(set({ id: payload, key: 'nonce', value: nonce }));
--- src/account/sagas/unit.test.ts
+++ src/account/sagas/unit.test.ts
@@ -3,16 +3,27 @@ import { testSaga } from 'redux-saga-test-plan';
 import Web3 from 'web3';
 import ganache from 'ganache-core';
 
-import { name } from '../common';
-import { selectByIdSingle } from '../selectors';
-
-import fetchBalanceAction from '../actions/fetchBalance';
-
-import exists from './exists';
-import fetchBalance from './fetchBalance';
 import networkExists from '../../network/sagas/networkExists';
 import { ZERO_ADDRESS } from '../../utils';
 import { Network } from '../../network/model';
+
+import { name } from '../common';
+import { selectByIdSingle } from '../selectors';
+
+//Actions
+import fetchBalanceAction from '../actions/fetchBalance';
+import fetchNonceAction from '../actions/fetchNonce';
+//import fetchBalanceSyncedAction from '../actions/fetchBalanceSynced'
+//import fetchNonceSyncedAction from '../actions/fetchNonceSynced';
+
+import setAction from '../actions/set';
+
+//Sagas
+import exists from './exists';
+import fetchBalance from './fetchBalance';
+import fetchNonce from './fetchNonce';
+//import fetchBalanceSynced from './fetchBalanceSynced';
+//import fetchNonceSynced from './fetchNonceSynced';
 
 describe(`${name}.sagas`, () => {
     const networkId = '1337';
@@ -44,14 +55,32 @@ describe(`${name}.sagas`, () => {
     });
 
     describe('fetchBalance', () => {
-        it('fetch', () => {
+        //TODO: Add error handling for web3 api
+        it('success', () => {
             testSaga(fetchBalance, fetchBalanceAction(id))
                 .next()
                 .call(exists, id)
                 .next(itemWithId)
                 .call(networkExists, networkId)
                 .next(network)
-                .call(network.web3!.eth.getBalance, item.address);
+                .call(network.web3!.eth.getBalance, item.address)
+                .next('0')
+                .put(setAction({ id, key: 'balance', value: '0' }));
+        });
+    });
+
+    describe('fetchNonce', () => {
+        //TODO: Add error handling for web3 api
+        it('success', () => {
+            testSaga(fetchNonce, fetchNonceAction(id))
+                .next()
+                .call(exists, id)
+                .next(itemWithId)
+                .call(networkExists, networkId)
+                .next(network)
+                .call(network.web3!.eth.getTransactionCount, item.address)
+                .next('0')
+                .put(setAction({ id, key: 'nonce', value: '0' }));
         });
     });
 });

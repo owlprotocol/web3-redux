336cac818815ef012ff0b340cba493f7bd96a854
Leo Vigna <leo.vigna@gmail.com>
2022-02-09 19:52:16 +0100

    remove unused ipfUrl field

--- src/ipfs/model/interface.ts
+++ src/ipfs/model/interface.ts
@@ -6,8 +6,6 @@ export interface IpfsId {
 export interface Ipfs extends IpfsId {
     /** data blob */
     readonly data?: Uint8Array;
-    /** Url */
-    readonly ipfsUrl?: string;
 }
 
 export default Ipfs;

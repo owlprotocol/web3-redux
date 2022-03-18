941154b65a36b661569d957f9d589d9b4ab96a7d
Nate <netanelbeny@gmail.com>
2022-03-18 12:51:30 +0200

    Add nft card

---
+++ packages/web3-redux-components/src/components/SingleNFTInstance/SingleNFTInstance.stories.tsx
@@ -0,0 +1,22 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { withThemeProvider } from '../../hoc';
+import SingleNFTInstance from '.';
+
+const Wrapper = withThemeProvider((props: any) => {
+    return <SingleNFTInstance {...props} />;
+});
+
+export default {
+    title: 'NFT/SingleNFTInstance',
+    component: SingleNFTInstance,
+} as ComponentMeta<typeof SingleNFTInstance>;
+
+const Template: ComponentStory<typeof SingleNFTInstance> = (args: any) => <Wrapper {...args} />;
+
+export const Main = Template.bind({});
+
+Main.args = {
+    itemName: 'Name',
+    owner: 'Owner',
+    price: '0.050',
+};
---
+++ packages/web3-redux-components/src/components/SingleNFTInstance/index.tsx
@@ -0,0 +1,81 @@
+import styled from 'styled-components';
+import Icon from '../Icon';
+
+const Wrapper = styled.div`
+    background: #1C1C24;
+    border-radius: 12px;
+    height: 346px;
+    width: 264px;
+    padding: 16px;
+    box-shadow: inset 0px 1px 4px 0px rgb(95 99 109 / 57%);
+`;
+
+const Item = styled.div`
+    background: #2C2F33;
+    border-radius: 12px;
+    width: 100%;
+    height: 196px;
+    margin-bottom: 16px;
+`;
+
+const Flex = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 8px;
+`;
+
+const Name = styled.div`
+    color: #FFFFFF;
+    font-weight: 700;
+    font-size: 14px;
+    line-height: 20px;
+    text-align: center;
+    padding: 6px;
+    border: 2px solid #2D2D3A;
+    box-sizing: border-box;
+    border-radius: 12px;
+    margin-bottom: 16px;
+`;
+
+const Avatar = styled.div`
+    background: #2C2F33;
+    margin-right: 8px;
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+    float: left;
+`;
+
+const Owner = styled.div`
+    color: #92929D;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+`;
+
+const Price = styled.div`
+    color: #92929D;
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 24px;
+`;
+
+const SingleNFTInstance = ({ itemName, owner, price }) => (
+    <Wrapper>
+        <Item />
+        <Name>{itemName}</Name>
+        <Flex>
+            <Owner>
+                <Avatar />
+                {owner}
+            </Owner>
+        </Flex>
+        <Flex>
+            <Price>{price} ETH</Price>
+            <Icon icon="ETH" />
+        </Flex>
+    </Wrapper>
+);
+
+export default SingleNFTInstance;

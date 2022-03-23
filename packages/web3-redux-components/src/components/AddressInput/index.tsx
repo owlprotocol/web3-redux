a5897ebd51f66f64ab082898e6d617d6c508be38
Nate <netanelbeny@gmail.com>
2022-03-23 13:07:47 +0200

    WIP add input wrapper

---
+++ packages/web3-redux-components/src/components/AddressInput/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button``;
+
+const Wrapper = styled.div`
+    background: #2C2C30;
+    border-radius: 8px;
+    height: 52px;
+    padding: 16px;
+    display: flex;
+    align-items: center;
+
+    input {
+        background: transparent;
+        border: 0;
+        width: 100%;
+        color: #fff;
+    }
+`;
+
+const ValidationMsg = styled.div`
+    color: rgba(255, 4, 32, 0.64);
+    font-weight: 400;
+    font-size: 14px;
+    margin: 1px 0 0 18px;
+`;
+
+export interface Props {
+    onChange: () => any;
+}
+
+const AddressInput = ({ onChange }: Props) => (
+    <>
+        <Wrapper>
+            <input type="text" onChange={onChange} placeholder="Enter address" />
+            <Button />
+            <Button />
+        </Wrapper>
+        <ValidationMsg>*Invalid address</ValidationMsg>
+    </>
+);
+
+export default AddressInput;

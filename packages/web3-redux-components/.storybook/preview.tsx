063ddb095f63872745d67cbdab97891caac326e5
Nate <netanelbeny@gmail.com>
2022-04-03 17:08:56 +0300

    cleanup

--- packages/web3-redux-components/.storybook/preview.tsx
+++ packages/web3-redux-components/.storybook/preview.tsx
@@ -14,14 +14,6 @@ import { withMockData } from '../src/hoc';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../src/theme';
 
-const colors = {
-    brand: {
-        900: '#1a365d',
-        800: '#153e75',
-        700: '#2a69ac',
-    },
-}
-
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {

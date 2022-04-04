1a8cf4981b0524a8164536bea4459507e87c7849
Nate <netanelbeny@gmail.com>
2022-04-04 13:01:05 +0300

    eslint

--- packages/web3-redux-components/src/components/AddressDisplay/index.tsx
+++ packages/web3-redux-components/src/components/AddressDisplay/index.tsx
@@ -1,15 +1,15 @@
 import { useEffect, useState } from 'react';
-import { useTheme } from '@chakra-ui/react'
+import { useTheme } from '@chakra-ui/react';
 import styled from 'styled-components';
 import copy from 'copy-to-clipboard';
+import { ReactComponent as HeartIcon } from './assets/heart.svg';
+import { ReactComponent as HeartActiveIcon } from './assets/heart-active.svg';
+import { ReactComponent as PencilIcon } from './assets/pencil.svg';
+import { ReactComponent as CopyIcon } from './assets/copy.svg';
 import QRCodePopover from '../QRCodePopover';
-import { ReactComponent as HeartIcon } from './assets/heart.svg'
-import { ReactComponent as HeartActiveIcon } from './assets/heart-active.svg'
-import { ReactComponent as PencilIcon } from './assets/pencil.svg'
-import { ReactComponent as CopyIcon } from './assets/copy.svg'
 
 const Wrapper = styled.div`
-    background: #2C2C30;
+    background: #2c2c30;
     border-radius: 12px;
     height: 60px;
     width: 100%;
@@ -26,9 +26,9 @@ const Wrapper = styled.div`
     input {
         flex: 1;
         background-color: transparent;
-        border-bottom: 1px solid #92929D;
+        border-bottom: 1px solid #92929d;
         margin-right: 12px;
-        color: #92929D;
+        color: #92929d;
     }
 `;
 
@@ -36,7 +36,7 @@ const Address = styled.div`
     font-weight: 600;
     font-size: 14px;
     line-height: 24px;
-    color: #92929D;
+    color: #92929d;
     flex: 1;
     word-break: break-all;
 `;
@@ -54,7 +54,7 @@ const Controls = styled.div`
 const SaveButton = styled.button`
     background: rgba(0, 102, 255, 0.16);
     border-radius: 28px;
-    color: #0066FF;
+    color: #0066ff;
     font-weight: 400;
     font-size: 14px;
     line-height: 19px;
@@ -64,7 +64,7 @@ const SaveButton = styled.button`
 `;
 
 const CancelButton = styled.button`
-    color: #92929D;
+    color: #92929d;
     font-weight: 400;
     font-size: 14px;
     line-height: 19px;
@@ -83,64 +83,74 @@ const AddressDisplay = ({ address, label, isFavorite }: Props) => {
     const [_isFavorite, setFavorite] = useState(false);
 
     const theme = useTheme();
-    console.log({theme});
-
+    console.log({ theme });
 
     useEffect(() => {
-        setLabel(label)
-        setFavorite(isFavorite)
-    }, [label, isFavorite])
+        setLabel(label);
+        setFavorite(isFavorite);
+    }, [label, isFavorite]);
 
     const handleInputKeyDown = (e: any) => {
         if (e.key === 'Enter') {
-            handleSave()
+            handleSave();
         }
-    }
+    };
 
     const handleSave = () => {
-        setEditLabel(false)
-    }
+        setEditLabel(false);
+    };
 
     const handleFavorite = () => {
-        setFavorite(!_isFavorite)
-    }
+        setFavorite(!_isFavorite);
+    };
 
     const handleCopy = () => {
         copy(address);
-    }
+    };
 
     return (
         <Wrapper>
             <QRCodePopover address={address} />
 
-            {editLabel &&
-                <input type="text" placeholder="Label" onChange={({target}) => setLabel(target.value)}
-                    onKeyDown={handleInputKeyDown} />}
+            {editLabel && (
+                <input
+                    type="text"
+                    placeholder="Label"
+                    onChange={({ target }) => setLabel(target.value)}
+                    onKeyDown={handleInputKeyDown}
+                />
+            )}
 
-            {!editLabel &&
+            {!editLabel && (
                 <Address>
-                    {_label
-                        ? <div>
+                    {_label ? (
+                        <div>
                             {_label} &lt; {address} &gt;
                         </div>
-                        : <div>{address}</div>}
-                </Address>}
+                    ) : (
+                        <div>{address}</div>
+                    )}
+                </Address>
+            )}
 
-            {editLabel ?
+            {editLabel ? (
                 <div>
                     <SaveButton onClick={handleSave}>Save</SaveButton>
                     <CancelButton onClick={() => setEditLabel(false)}>Cancel</CancelButton>
                 </div>
-            :
+            ) : (
                 <Controls>
-                    <button onClick={handleCopy}><CopyIcon /></button>
-                    <button onClick={handleFavorite}>
-                        {_isFavorite ? <HeartActiveIcon /> : <HeartIcon />}
+                    <button onClick={handleCopy}>
+                        <CopyIcon />
                     </button>
-                    <button onClick={() => setEditLabel(true)}><PencilIcon /></button>
-                </Controls>}
+                    <button onClick={handleFavorite}>{_isFavorite ? <HeartActiveIcon /> : <HeartIcon />}</button>
+                    <button onClick={() => setEditLabel(true)}>
+                        <PencilIcon />
+                    </button>
+                </Controls>
+            )}
         </Wrapper>
     );
-}
+};
 
 export default AddressDisplay;

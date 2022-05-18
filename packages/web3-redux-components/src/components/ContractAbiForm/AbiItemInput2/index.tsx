e71605c8cf3f78903d11f924762039bea6eef15a
Leo Vigna <leo.vigna@gmail.com>
2022-05-19 03:17:04 +0400

    AbiForm2

--- packages/web3-redux-components/src/components/ContractAbiForm/AbiItemInput2/index.tsx
+++ packages/web3-redux-components/src/components/ContractAbiForm/AbiItemInput2/index.tsx
@@ -1,15 +1,44 @@
 import { useCallback, useState } from 'react';
 import Web3 from 'web3';
-import { Input, NumberInput, Checkbox, NumberDecrementStepper, NumberIncrementStepper, NumberInputField, NumberInputStepper, FormErrorMessage, FormControl, FormLabel } from '@chakra-ui/react'
+import {
+    Input,
+    NumberInput,
+    Checkbox,
+    NumberDecrementStepper,
+    NumberIncrementStepper,
+    NumberInputField,
+    NumberInputStepper,
+    FormErrorMessage,
+    FormControl,
+    FormLabel,
+} from '@chakra-ui/react';
 
-const web3 = new Web3()
+const web3 = new Web3();
 const coder = web3.eth.abi;
 
-type AbiItemInputType = 'string' | 'address' | 'boolean' | 'bytes' |
-    'bytes32' | 'byte16' | 'bytes8' | 'bytes4' |
-    'uint256' | 'uint128' | 'uint64' | 'uint32' | 'uint16' | 'uint8' | 'uint4' |
-    'int256' | 'int128' | 'int64' | 'int32' | 'int16' | 'int8' | 'int4'
-
+type AbiItemInputType =
+    | 'string'
+    | 'address'
+    | 'boolean'
+    | 'bytes'
+    | 'bytes32'
+    | 'byte16'
+    | 'bytes8'
+    | 'bytes4'
+    | 'uint256'
+    | 'uint128'
+    | 'uint64'
+    | 'uint32'
+    | 'uint16'
+    | 'uint8'
+    | 'uint4'
+    | 'int256'
+    | 'int128'
+    | 'int64'
+    | 'int32'
+    | 'int16'
+    | 'int8'
+    | 'int4';
 
 export interface Props {
     type: AbiItemInputType;
@@ -29,40 +58,45 @@ const AbiItemInput = ({ type, name, onChange = () => { } }: Props) => {
     if (type.startsWith('uint') || type.startsWith('int')) inputType = 'number';
     else if (type === 'boolean') inputType = 'boolean';
 
-    const onChangeValidate = useCallback((_value: string | boolean) => {
-        console.debug({ _value })
-        if (typeof _value == 'boolean') {
-            setError(undefined); setValue(_value);
-            onChange(value, error);
-            return;
-        }
+    const onChangeValidate = useCallback(
+        (_value: string | boolean) => {
+            if (typeof _value == 'boolean') {
+                setError(undefined);
+                setValue(_value);
+                onChange(_value, undefined);
+                return;
+            }
 
-        //Empty
-        if (_value.length == 0) {
-            setError(undefined); setValue(undefined);
-            onChange(value, error);
-            return;
-        }
+            //Empty
+            if (_value.length == 0) {
+                setError(undefined);
+                setValue(undefined);
+                onChange(undefined, undefined);
+                return;
+            }
 
-        //Address
-        //TODO: Update input field value
-        if (type === 'address' && Web3.utils.isAddress(_value.toLowerCase())) {
-            _value = Web3.utils.toChecksumAddress(_value);
-        }
+            //Address
+            //TODO: Update input field value
+            if (type === 'address' && Web3.utils.isAddress(_value.toLowerCase())) {
+                _value = Web3.utils.toChecksumAddress(_value);
+            }
 
-        //Validate
-        try {
-            coder.encodeParameter(type, _value);
-        } catch (_error: any) {
-            setError(_error); setValue(_value)
-            onChange(_value, _error)
-            return;
-        }
-
-        //Format address if valid
-        setError(undefined); setValue(_value)
-        onChange(value, error);
-    }, [onChange]);
+            //Validate
+            try {
+                coder.encodeParameter(type, _value);
+                //Format is valid
+                setError(undefined);
+                setValue(_value);
+                onChange(_value, undefined);
+            } catch (_error: any) {
+                setError(_error);
+                setValue(_value);
+                onChange(_value, _error);
+                return;
+            }
+        },
+        [onChange],
+    );
 
     //Custom NumberInput Add-on to add unit selector
     return (
@@ -70,28 +104,31 @@ const AbiItemInput = ({ type, name, onChange = () => { } }: Props) => {
             <FormControl isInvalid={!!error}>
                 {
                     {
-                        'string': <Input placeholder={placeholder}
-                            value={value as string | undefined}
-                            onChange={({ target }) => onChangeValidate(target.value)}
-                        />,
-                        'boolean': <>
-                            <FormLabel>{placeholder}</FormLabel>
-                            <Checkbox
-                                onChange={({ target }) => onChangeValidate(target.checked)} />
-                        </>,
-                        'number': <NumberInput
-                            onChange={onChangeValidate}>
-                            <NumberInputField placeholder={placeholder} />
-                            <NumberInputStepper>
-                                <NumberIncrementStepper />
-                                <NumberDecrementStepper />
-                            </NumberInputStepper>
-                        </NumberInput>
+                        string: (
+                            <Input
+                                placeholder={placeholder}
+                                value={value as string | undefined}
+                                onChange={({ target }) => onChangeValidate(target.value)}
+                            />
+                        ),
+                        boolean: (
+                            <>
+                                <FormLabel>{placeholder}</FormLabel>
+                                <Checkbox onChange={({ target }) => onChangeValidate(target.checked)} />
+                            </>
+                        ),
+                        number: (
+                            <NumberInput onChange={onChangeValidate}>
+                                <NumberInputField placeholder={placeholder} />
+                                <NumberInputStepper>
+                                    <NumberIncrementStepper />
+                                    <NumberDecrementStepper />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        ),
                     }[inputType]
                 }
-                {
-                    error && <FormErrorMessage>{error.message}</FormErrorMessage>
-                }
+                <FormErrorMessage>{error?.message}</FormErrorMessage>
             </FormControl>
         </>
     );
--- packages/web3-redux-components/src/types/modules.d.ts
+++ packages/web3-redux-components/src/types/modules.d.ts
@@ -2,3 +2,6 @@ declare module 'redux-orm';
 declare module 'ethereum-ens';
 declare module 'react-hooks-compose';
 declare module 'ethereum-qr-code';
+declare module 'uuid' {
+    export function v4(): string;
+}

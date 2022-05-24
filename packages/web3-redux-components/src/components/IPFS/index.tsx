8a0b38f0ea0610844c28a4f2978c7d4169c245ab
Leo Vigna <leo.vigna@gmail.com>
2022-05-24 12:19:04 +0400

    refactor IPFS components

--- packages/web3-redux-components/src/components/IPFS/index.tsx
+++ packages/web3-redux-components/src/components/IPFS/index.tsx
@@ -1,17 +1,1 @@
-import { Button } from '@chakra-ui/react';
-import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import { Ipfs } from '@owlprotocol/web3-redux';
-
-export const IPFSButton = () => {
-    const dispatch = useDispatch();
-
-    const onClick = useCallback(() => {
-        const payload = { message: 'Test' };
-        dispatch(Ipfs.putCBOR(payload));
-    }, [dispatch]);
-
-    return <Button onClick={onClick}>Create IPFS</Button>;
-};
-
-export default IPFSButton;
+export { IPFSWriteJSON } from './IPFSWriteJSON';

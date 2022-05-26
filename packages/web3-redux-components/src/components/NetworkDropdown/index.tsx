b8f8284bc192dcebda8f9b91496ae1bce722ce25
Nate <netanelbeny@gmail.com>
2022-05-26 12:07:08 +0300

    wip

--- packages/web3-redux-components/src/components/NetworkDropdown/index.tsx
+++ packages/web3-redux-components/src/components/NetworkDropdown/index.tsx
@@ -3,19 +3,29 @@ import { Box, useTheme } from '@chakra-ui/react';
 import Dropdown from '../Dropdown';
 import NetworkIcon from '../NetworkIcon';
 
+const DEFAULT_CHAINS = ['ethereum', 'arbitrum', 'optimism', 'polygon', 'BNB Chain'];
+
 export interface Props {
-    options: string[];
+    options?: string[];
+    handleChange?: any;
 }
-export const NetworkDropdown = ({ options = [] }: Props) => {
+export const NetworkDropdown = ({ options = [], handleChange }: Props) => {
     const { themes } = useTheme();
     const [selectedNetwork, setSelectedNetwork] = useState('1');
+    const _options = [...DEFAULT_CHAINS, options];
+
+    const _onChange = (value: string) => {
+        setSelectedNetwork(value);
+        handleChange(value, 'networkId');
+    };
 
     return (
-        <Box display={'flex'} alignItems={'center'} borderRadius={4} bg={themes.color6} color={themes.color8}>
-            <Box p={2}>
+        <Box display={'flex'} alignItems={'center'} borderRadius={12} bg={themes.color6} color={themes.color8}>
+            <Box p={2} w={'20px'} h={'20px'}>
                 <NetworkIcon networkId={selectedNetwork} />
             </Box>
-            <Dropdown placeholder="Select a network" options={options} onChange={setSelectedNetwork} />
+            {/* @ts-ignore */}
+            <Dropdown placeholder="Select a network" options={_options} onChange={_onChange} />
         </Box>
     );
 };

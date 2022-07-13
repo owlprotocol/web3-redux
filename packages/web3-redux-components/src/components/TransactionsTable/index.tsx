465d727c55b3ce8444eac7e88781dc396d2d4ff1
Ashutosh Pandey <ashutosh.p@hiehq.com>
2022-07-13 17:49:31 +0530

    feat(Table): UI updated

--- packages/web3-redux-components/src/components/TransactionsTable/index.tsx
+++ packages/web3-redux-components/src/components/TransactionsTable/index.tsx
@@ -2,12 +2,33 @@ import { useTheme, TableContainer, Table, Thead, Tbody, Tr, Td, Th, Badge } from
 import styled from '@emotion/styled';
 
 const TableWrapper = styled.div`
+    table {
+        border-spacing: 0;
+        border-collapse: seperate;
+    }
+
     table tr:nth-child(odd) {
         background-color: #1c1c24;
     }
 
     table tr:nth-child(even) {
         background-color: #2c2c30;
+    }
+
+    table th:first-child {
+        border-top-left-radius: 12px;
+    }
+
+    table th:last-child {
+        border-top-right-radius: 12px;
+    }
+
+    table tr:last-child td:first-child {
+        border-bottom-left-radius: 12px;
+    }
+
+    table tr:last-child td:last-child {
+        border-bottom-right-radius: 12px;
     }
 `;
 

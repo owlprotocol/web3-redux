48c1d57454f2618d81f075c93410c30f5b607928
Leo Vigna <leo.vigna@gmail.com>
2022-07-14 20:01:50 +0400

    network on update hydrate contracts

--- packages/web3-redux/src/network/middleware/onUpdate copy.ts
+++ packages/web3-redux/src/network/middleware/onUpdate copy.ts
@@ -9,29 +9,46 @@ export const onNetworkUpdate = (store: Store) => (next: (action: AnyAction) => a
     let networkId;
     let newWeb3: Web3 | undefined;
     let newWeb3Sender: Web3 | undefined;
-    let newWeb3Rpc: string | undefined;
     if (NetworkCRUD.actions.create.match(action) || NetworkCRUD.actions.update.match(action)) {
         networkId = action.payload.networkId;
         newWeb3 = action.payload.web3;
-        newWeb3Rpc = action.payload.web3Rpc;
         newWeb3Sender = action.payload.web3Sender;
     }
 
     const network = NetworkCRUD.selectors.selectByIdSingle(store.getState(), networkId);
-    const web3Changed = (!!newWeb3 && network?.web3 !== newWeb3) || (newWeb3Rpc && network?.web3Rpc !== newWeb3Rpc);
+    const web3Changed = !!newWeb3 && network?.web3 !== newWeb3;
     const web3SenderChanged = !!newWeb3Sender && network?.web3Sender !== newWeb3Sender;
-
     next(action); //Create/Update network
 
     const contracts = ContractCRUD.selectors.selectWhere(store.getState(), { networkId }) ?? [];
-    const contractIds = contracts.map(({ networkId, address }) => {
-        return { networkId, address };
-    });
 
-    console.log(`Network ${networkId} updated. Hydrating ${JSON.stringify(contractIds)}`);
+    if (web3Changed) {
+        const updates = contracts
+            .filter((c) => !!c.abi)
+            .map((c) => {
+                return {
+                    networkId: c.networkId,
+                    address: c.address,
+                    web3Contract: new newWeb3!.eth.Contract(c.abi!, c.address),
+                };
+            });
 
-    if ((web3Changed || web3SenderChanged) && contractIds.length > 0)
-        store.dispatch(ContractCRUD.actions.hydrateBatched(contractIds));
+        if (updates.length > 0) store.dispatch(ContractCRUD.actions.updateBatched(updates));
+    }
+
+    if (web3SenderChanged) {
+        const updates = contracts
+            .filter((c) => !!c.abi)
+            .map((c) => {
+                return {
+                    networkId: c.networkId,
+                    address: c.address,
+                    web3SenderContract: new newWeb3Sender!.eth.Contract(c.abi!, c.address),
+                };
+            });
+
+        if (updates.length > 0) store.dispatch(ContractCRUD.actions.updateBatched(updates));
+    }
 };
 
 export default onNetworkUpdate;

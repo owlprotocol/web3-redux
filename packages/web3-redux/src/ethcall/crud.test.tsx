a963c0b39101e8c881580ae853cc28132821c1d6
Leo Vigna <leo.vigna@gmail.com>
2022-07-14 18:39:00 +0400

    ethcall crud tests

--- packages/web3-redux/src/ethcall/crud.test.tsx
+++ packages/web3-redux/src/ethcall/crud.test.tsx
@@ -3,18 +3,15 @@ import { assert } from 'chai';
 import { Provider } from 'react-redux';
 import { renderHook } from '@testing-library/react-hooks';
 import { name } from './common.js';
-import { ethCall1 } from './data.js';
+import { ethCall1, ethCall2 } from './data.js';
 import EthCallCRUD from './crud.js';
 import { createStore, StoreType } from '../store.js';
 import expectThrowsAsync from '../test/expectThrowsAsync.js';
 
 describe(`${name}/crud.test.js`, () => {
-    describe('store', () => {
-        let store: StoreType;
-
+    describe('db', () => {
         beforeEach(async () => {
-            store = createStore();
-            store.dispatch(EthCallCRUD.actions.create(ethCall1));
+            await EthCallCRUD.db.add(ethCall1);
         });
 
         it('get({networkId,to,data})', async () => {
@@ -51,93 +48,167 @@ describe(`${name}/crud.test.js`, () => {
             assert.deepEqual(selected, [ethCall1]);
         });
 
-        describe('hooks', () => {
-            let wrapper: any;
-            beforeEach(() => {
-                wrapper = ({ children }: any) => <Provider store={store}> {children} </Provider>;
-            });
+        it('where({networkId},{limit:1})', async () => {
+            await EthCallCRUD.db.add(ethCall2);
+            const selected = await EthCallCRUD.db.where(
+                {
+                    networkId: ethCall1.networkId!,
+                },
+                { limit: 1 },
+            );
+            assert.deepEqual(selected, [ethCall1]);
+        });
 
-            it('useGet({networkId,to,data})', async () => {
-                const { result, waitForNextUpdate } = renderHook(
-                    () =>
-                        EthCallCRUD.hooks.useGet({
+        it('where({networkId},{limit:1,offset:1})', async () => {
+            await EthCallCRUD.db.add(ethCall2);
+            const selected = await EthCallCRUD.db.where(
+                {
+                    networkId: ethCall1.networkId!,
+                },
+                { limit: 1, offset: 1 },
+            );
+            assert.deepEqual(selected, [ethCall2]);
+        });
+    });
+
+    describe('hooks', () => {
+        let store: StoreType;
+        let wrapper: any;
+
+        beforeEach(async () => {
+            store = createStore();
+            store.dispatch(EthCallCRUD.actions.create(ethCall1));
+            wrapper = ({ children }: any) => <Provider store={store}> {children} </Provider>;
+        });
+
+        it('useGet({networkId,to,data})', async () => {
+            const { result, waitForNextUpdate } = renderHook(
+                () =>
+                    EthCallCRUD.hooks.useGet({
+                        networkId: ethCall1.networkId,
+                        to: ethCall1.to,
+                        data: ethCall1.data,
+                    }),
+                {
+                    wrapper,
+                },
+            );
+
+            await waitForNextUpdate();
+            const [selected] = result.current;
+            assert.deepEqual(selected, ethCall1);
+
+            //No additional re-renders frm background tasks
+            await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
+        });
+
+        it('useGet({methodName})', async () => {
+            const { result, waitForNextUpdate } = renderHook(
+                () =>
+                    EthCallCRUD.hooks.useGet({
+                        methodName: ethCall1.methodName!,
+                    }),
+                {
+                    wrapper,
+                },
+            );
+
+            await waitForNextUpdate();
+            const [selected] = result.current;
+            assert.deepEqual(selected, ethCall1);
+
+            //No additional re-renders frm background tasks
+            await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
+        });
+
+        it('useGetBulk([{networkId,to,data}])', async () => {
+            const { result, waitForNextUpdate } = renderHook(
+                () =>
+                    EthCallCRUD.hooks.useGetBulk([
+                        {
                             networkId: ethCall1.networkId,
                             to: ethCall1.to,
                             data: ethCall1.data,
-                        }),
-                    {
-                        wrapper,
-                    },
-                );
+                        },
+                    ]),
+                {
+                    wrapper,
+                },
+            );
 
-                await waitForNextUpdate();
-                const [selected] = result.current;
-                assert.deepEqual(selected, ethCall1);
+            await waitForNextUpdate();
+            const [selected] = result.current;
+            assert.deepEqual(selected, [ethCall1]);
 
-                //No additional re-renders frm background tasks
-                await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
-            });
+            //No additional re-renders frm background tasks
+            await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
+        });
 
-            it('useGet({methodName})', async () => {
-                const { result, waitForNextUpdate } = renderHook(
-                    () =>
-                        EthCallCRUD.hooks.useGet({
-                            methodName: ethCall1.methodName!,
-                        }),
-                    {
-                        wrapper,
-                    },
-                );
+        it('useWhere({methodName})', async () => {
+            const { result, waitForNextUpdate } = renderHook(
+                () =>
+                    EthCallCRUD.hooks.useWhere({
+                        methodName: ethCall1.methodName!,
+                    }),
+                {
+                    wrapper,
+                },
+            );
 
-                await waitForNextUpdate();
-                const [selected] = result.current;
-                assert.deepEqual(selected, ethCall1);
+            await waitForNextUpdate();
+            const [selected] = result.current;
+            assert.deepEqual(selected, [ethCall1]);
 
-                //No additional re-renders frm background tasks
-                await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
-            });
+            //No additional re-renders frm background tasks
+            await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
+        });
 
-            it('useGetBulk([{networkId,to,data}])', async () => {
-                const { result, waitForNextUpdate } = renderHook(
-                    () =>
-                        EthCallCRUD.hooks.useGetBulk([
-                            {
-                                networkId: ethCall1.networkId,
-                                to: ethCall1.to,
-                                data: ethCall1.data,
-                            },
-                        ]),
-                    {
-                        wrapper,
-                    },
-                );
+        it('useWhere({networkId,{limit:1}})', async () => {
+            store.dispatch(EthCallCRUD.actions.create(ethCall2));
 
-                await waitForNextUpdate();
-                const [selected] = result.current;
-                assert.deepEqual(selected, [ethCall1]);
+            const { result, waitForNextUpdate } = renderHook(
+                () =>
+                    EthCallCRUD.hooks.useWhere(
+                        {
+                            networkId: ethCall1.networkId!,
+                        },
+                        { limit: 1 },
+                    ),
+                {
+                    wrapper,
+                },
+            );
 
-                //No additional re-renders frm background tasks
-                await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
-            });
+            await waitForNextUpdate();
+            const [selected] = result.current;
+            assert.deepEqual(selected, [ethCall1]);
 
-            it('useWhere({methodName})', async () => {
-                const { result, waitForNextUpdate } = renderHook(
-                    () =>
-                        EthCallCRUD.hooks.useWhere({
-                            methodName: ethCall1.methodName!,
-                        }),
-                    {
-                        wrapper,
-                    },
-                );
+            //No additional re-renders frm background tasks
+            await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
+        });
 
-                await waitForNextUpdate();
-                const [selected] = result.current;
-                assert.deepEqual(selected, [ethCall1]);
+        it('useWhere({networkId,{limit:1,offset:1}})', async () => {
+            store.dispatch(EthCallCRUD.actions.create(ethCall2));
 
-                //No additional re-renders frm background tasks
-                await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
-            });
+            const { result, waitForNextUpdate } = renderHook(
+                () =>
+                    EthCallCRUD.hooks.useWhere(
+                        {
+                            networkId: ethCall1.networkId!,
+                        },
+                        { limit: 1, offset: 1 },
+                    ),
+                {
+                    wrapper,
+                },
+            );
+
+            await waitForNextUpdate();
+            const [selected] = result.current;
+            assert.deepEqual(selected, [ethCall2]);
+
+            //No additional re-renders frm background tasks
+            await expectThrowsAsync(waitForNextUpdate, 'Timed out in waitForNextUpdate after 1000ms.');
         });
     });
 });
--- packages/web3-redux/src/ethcall/data.ts
+++ packages/web3-redux/src/ethcall/data.ts
@@ -2,7 +2,7 @@ import type { AbiItem } from 'web3-utils';
 import EthCallCRUD from './crud.js';
 import { coder } from '../utils/web3-eth-abi/index.js';
 import { BlockNumberArtifact } from '../abis/index.js';
-import { networkId, ADDRESS_1 } from '../data.js';
+import { networkId, ADDRESS_1, ADDRESS_2 } from '../data.js';
 
 //Ethcall
 const methodName = 'getValue';
@@ -11,6 +11,14 @@ const data = coder.encodeFunctionCall(methodAbi, []);
 export const ethCall1 = EthCallCRUD.validate({
     networkId,
     to: ADDRESS_1,
+    data,
+    methodName,
+    returnValue: 66,
+});
+
+export const ethCall2 = EthCallCRUD.validate({
+    networkId,
+    to: ADDRESS_2,
     data,
     methodName,
     returnValue: 66,

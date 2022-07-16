d73ae9de2a9d1fdbe5cfbcec81b7fc9fce613f04
Leo Vigna <leo.vigna@gmail.com>
2022-07-16 17:29:38 +0400

    getNonce & test

--- packages/web3-redux/src/contract/sagas/getNonce.test.ts
+++ packages/web3-redux/src/contract/sagas/getNonce.test.ts
@@ -1,41 +1,54 @@
 import { assert } from 'chai';
-import Web3 from 'web3';
-import { getWeb3Provider } from '../../test/index.js';
+import { testSaga } from 'redux-saga-test-plan';
+import getNonce from './getNonce.js';
 
-import { networkId } from '../../test/data.js';
+import { ADDRESS_0 } from '../../test/data.js';
 import { createStore, StoreType } from '../../store.js';
 
-import { Contract } from '../model/interface.js';
 import { name } from '../common.js';
 
 import { getNonce as getNonceAction } from '../actions/index.js';
-import ContractCRUD from '../crud.js';
 import NetworkCRUD from '../../network/crud.js';
+import ContractCRUD from '../crud.js';
+import { network1336 } from '../../network/data.js';
+import loadNetwork from '../../network/sagas/loadNetwork.js';
+import sleep from '../../utils/sleep.js';
 
-describe(`${name}.integration`, () => {
-    let store: StoreType;
-    let web3: Web3;
+const networkId = network1336.networkId;
+const web3 = network1336.web3!;
+const address = ADDRESS_0;
+const action = getNonceAction({ networkId, address }, '');
 
-    let item: Contract;
-
-    before(async () => {
-        const provider = getWeb3Provider();
-        //@ts-ignore
-        web3 = new Web3(provider);
-        const accounts = await web3.eth.getAccounts();
-        item = { networkId, address: accounts[0] };
+describe(`${name}/sagas/getNonce.test.ts`, () => {
+    describe('unit', () => {
+        it('getNonce', () => {
+            testSaga(getNonce, action)
+                .next()
+                .call(loadNetwork, networkId)
+                .next({ networkId, web3 })
+                .call(web3.eth.getTransactionCount, address)
+                .next('0')
+                .put(ContractCRUD.actions.upsert({ networkId, address, nonce: 0 }, ''));
+        });
     });
 
-    beforeEach(() => {
-        store = createStore();
-        store.dispatch(NetworkCRUD.actions.create({ networkId, web3 }));
-        store.dispatch(ContractCRUD.actions.create(item));
-    });
+    describe('store', () => {
+        let store: StoreType;
 
-    it('getNonce()', async () => {
-        store.dispatch(getNonceAction(item));
-        const expected = await web3.eth.getTransactionCount(item.address!);
-        const account = await ContractCRUD.db.get(item);
-        assert.equal(account!.nonce, expected, 'initial nonce');
+        beforeEach(async () => {
+            store = createStore();
+            store.dispatch(NetworkCRUD.actions.create({ networkId, web3 }));
+            store.dispatch(ContractCRUD.actions.create({ networkId, address }));
+        });
+
+        it('getNonce', async () => {
+            store.dispatch(getNonceAction({ networkId, address }));
+            await sleep(1000);
+
+            const expected = await web3.eth.getTransactionCount(address!);
+            const account = await ContractCRUD.db.get({ networkId, address });
+
+            assert.equal(account!.nonce, expected, 'initial nonce');
+        });
     });
 });
--- packages/web3-redux/src/contract/sagas/getNonce.ts
+++ packages/web3-redux/src/contract/sagas/getNonce.ts
@@ -1,4 +1,4 @@
-import { select, put, call } from 'typed-redux-saga';
+import { put, call } from 'typed-redux-saga';
 import { GetNonceAction } from '../actions/index.js';
 import ContractCRUD from '../crud.js';
 import loadNetwork from '../../network/sagas/loadNetwork.js';
@@ -16,12 +16,9 @@ export function* getNonce(action: GetNonceAction) {
         const web3 = network.web3 ?? network.web3Sender;
         if (!web3) throw new Error(`Network ${networkId} missing web3 or web3Sender`);
 
-        const contract = yield* select(ContractCRUD.selectors.selectByIdSingle, { networkId, address });
-        if (!contract) yield* put(ContractCRUD.actions.create({ networkId, address }));
-
         //@ts-expect-error
         const nonce: string = yield* call(web3.eth.getTransactionCount, address);
-        yield* put(ContractCRUD.actions.update({ networkId, address, nonce: parseInt(nonce) }));
+        yield* put(ContractCRUD.actions.upsert({ networkId, address, nonce: parseInt(nonce) }, action.meta.uuid));
     } catch (error) {
         yield* put(
             createError(

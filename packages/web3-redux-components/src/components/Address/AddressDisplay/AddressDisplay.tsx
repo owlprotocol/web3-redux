7df904d0fdfda251236293e4a96eb90c2040c544
Leo Vigna <leo.vigna@gmail.com>
2022-07-25 09:53:07 +0400

    AddressDisplay controls

--- packages/web3-redux-components/src/components/Address/AddressDisplay/AddressDisplay.tsx
+++ packages/web3-redux-components/src/components/Address/AddressDisplay/AddressDisplay.tsx
@@ -1,10 +1,12 @@
 import composeHooks from 'react-hooks-compose';
-import AddressDisplayPresenter from './AddressDisplayPresenter.js';
+import { StyleProps } from '@chakra-ui/react';
+import AddressDisplayPresenter, { Control } from './AddressDisplayPresenter.js';
 import { useFavorite } from '../AddressFavoriteButton/useFavorite.js';
 import { Address } from '../../../interfaces/Address.js';
 import { useLabel } from '../AddressLabel/useLabel.js';
 
-const useAddressDisplay = ({ networkId, address }: Partial<Address>) => {
+export type useAddressDisplayProps = Partial<Address>;
+const useAddressDisplay = ({ networkId, address }: useAddressDisplayProps) => {
     const [isFavorite, { toggleFavorite }] = useFavorite(networkId, address);
     const [label, { setLabel }] = useLabel(networkId, address);
 
@@ -14,9 +16,11 @@ const useAddressDisplay = ({ networkId, address }: Partial<Address>) => {
 export interface AddressDisplayProps extends Partial<Address> {
     borderRadius?: number;
     bg?: string;
+    controls?: Control[];
+    containerStyles?: StyleProps;
 }
-const AddressDisplay = composeHooks((props: Address) => ({
-    useAddressDisplay: () => useAddressDisplay(props),
+const AddressDisplay = composeHooks(({ networkId, address }: AddressDisplayProps) => ({
+    useAddressDisplay: () => useAddressDisplay({ networkId, address }),
 }))(AddressDisplayPresenter) as (props: AddressDisplayProps) => JSX.Element;
 
 //@ts-expect-error
